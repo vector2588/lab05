ecc095f59024dcc7b3712ffc4120e267c968b54e
Vector2588 <natkamol082@gmail.com>
2021-07-29 22:52:05 +0700

    4.4 Scoll up when change page

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -52,7 +52,7 @@ export default {
 
   created() {
     watchEffect(() => {
-      EventService.getEvents(2, this.page)
+      EventService.getEvents(3, this.page)
         .then((response) => {
           this.events = response.data
           this.totalEvents = response.headers['x-total-count'] // <--- Store it
@@ -66,7 +66,7 @@ export default {
   computed: {
     hasNextPage() {
       // First, calculate total pages
-      let totalPages = Math.ceil(this.totalEvents / 2) // 2 is events per page
+      let totalPages = Math.ceil(this.totalEvents / 3) // 2 is events per page
 
       // Then check to see if the current page is less than the total pages.
       return this.page < totalPages
@@ -75,7 +75,7 @@ export default {
 
   // eslint-disable-next-line no-unused-vars
   beforeRouteEnter(routeTo, routeFrom, next) {
-    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+    EventService.getEvents(3, parseInt(routeTo.query.page) || 1)
       .then((response) => {
         next((comp) => {
           comp.events = response.data
@@ -87,7 +87,7 @@ export default {
       })
   },
   beforeRouteUpdate(routeTo, routeFrom, next) {
-    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+    EventService.getEvents(3, parseInt(routeTo.query.page) || 1)
       .then((response) => {
         this.events = response.data // <-----
         this.totalEvents = response.headers['x-total-count'] // <-----
