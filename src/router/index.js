df3bd491cbe78736c8437b3bae369ec6e21ddd82
Vector2588 <natkamol082@gmail.com>
2021-07-29 22:35:14 +0700

    3.6 Show code

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import EventLayout from '@/views/event/Layout.vue'
 import NotFound from '@/views/NotFound.vue'
 import NetWorkError from '@/views/NetworkError.vue'
 import NProgress from 'nprogress'
+import EventService from '@/services/EventService.js'
+import GStore from '@/store'
 const routes = [
   {
     path: '/',
@@ -25,6 +27,23 @@ const routes = [
     name: 'EventLayout',
     props: true,
     component: EventLayout,
+    beforeEnter: (to) => {
+      return EventService.getEvent(to.params.id) // Return and params.id
+      .then((response) => {
+        // Still need to set the data here
+        GStore.event = response.data // <--- Store the event
+      })
+      .catch((error) => {
+        if (error.response && error.response.status == 404) {
+          return { // <--- Return
+            name: '404Resource',
+            params: { resource: 'event' }
+          }
+        } else {
+          return { name: 'NetworkError' } // <--- Return
+        }
+      })
+    },
     children: [
       {
         path: '',
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,2 @@
-import { createStore } from 'vuex'
-
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-})
+import { reactive } from 'vue'
+export default reactive({flashMessage: '',event:null})
